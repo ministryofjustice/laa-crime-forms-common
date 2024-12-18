90fee12c2abea2d65134743761565bcf1abae92b
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2024-12-18 10:21:04 +0000

    Standardised mechanism for S3 downloads

--- lib/laa_crime_forms_common.rb
+++ lib/laa_crime_forms_common.rb
@@ -4,9 +4,10 @@ require "laa_crime_forms_common/anonymiser"
 require "laa_crime_forms_common/assignment"
 require "laa_crime_forms_common/autogrant/prior_authority"
 require "laa_crime_forms_common/hooks"
-require "laa_crime_forms_common/pricing/nsm"
-require "laa_crime_forms_common/validator"
 require "laa_crime_forms_common/messages/prior_authority"
+require "laa_crime_forms_common/pricing/nsm"
+require "laa_crime_forms_common/s3_files"
+require "laa_crime_forms_common/validator"
 
 mydir = __dir__
 I18n.load_path += Dir[File.join(mydir, "locales", "**/*.yml")]
