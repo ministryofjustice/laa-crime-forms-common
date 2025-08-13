aa03793a6f23f0d04faf501075b99bbaf12ce0ee
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-08-13 10:57:53 +0100

    use correct module for number_to

--- lib/laa_crime_forms_common/number_to.rb
+++ lib/laa_crime_forms_common/number_to.rb
@@ -1,20 +1,22 @@
 require "active_support"
 
-module NumberTo
-  extend ActiveSupport::NumberHelper
+module LaaCrimeFormsCommon
+  module NumberTo
+    extend ActiveSupport::NumberHelper
 
-  def self.pounds(*values)
-    value = values.any?(&:nil?) ? nil : values.sum
-    return "£" unless value
+    def self.pounds(*values)
+      value = values.any?(&:nil?) ? nil : values.sum
+      return "£" unless value
 
-    number_to_currency(value, unit: "£")
-  end
+      number_to_currency(value, unit: "£")
+    end
 
-  def self.percentage(value, decimals: 0, multiplier: 100)
-    "#{(value * multiplier).round(decimals)}%"
-  end
+    def self.percentage(value, decimals: 0, multiplier: 100)
+      "#{(value * multiplier).round(decimals)}%"
+    end
 
-  def self.formatted(value)
-    number_to_rounded(value, precision: 1, strip_insignificant_zeros: true)
+    def self.formatted(value)
+      number_to_rounded(value, precision: 1, strip_insignificant_zeros: true)
+    end
   end
 end
