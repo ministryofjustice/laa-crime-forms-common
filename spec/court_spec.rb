289ad0ae87120e32d3f8bf47c62e16538d008f66
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2026-02-23 12:13:30 +0000

    Fix court_spec and upgrade ruby version

--- spec/court_spec.rb
+++ spec/court_spec.rb
@@ -7,20 +7,24 @@ RSpec.describe LaaCrimeFormsCommon::Court do
 
     it "returns required courts as expected" do
       digest_of_expected_court_names = "88aff34c81d39f306905cc7cbef807fa"
+      digest_of_expected_court_ids = "25ed0135c77f910c3ac9493c732ec806"
+      digest_of_expected_short_court_names = "518956c5e16fbede58bd3beba845a924"
 
       expect(Digest::MD5.hexdigest(subject.map(&:name).join)).to eq digest_of_expected_court_names
+      expect(Digest::MD5.hexdigest(subject.map(&:id).join)).to eq digest_of_expected_court_ids
+      expect(Digest::MD5.hexdigest(subject.map(&:short_name).join)).to eq digest_of_expected_short_court_names
     end
   end
 
   describe "#==" do
-    subject { described_class.new(name: "check_against") }
+    subject { described_class.new(id: 1, short_name: "check_against", name: "1 - check_against") }
 
     context "when other object is the same value" do
-      it { expect(subject).to eq(described_class.new(name: "check_against")) }
+      it { expect(subject).to eq(described_class.new(id: 1, short_name: "check_against", name: "1 - check_against")) }
     end
 
     context "when other object has a different value" do
-      it { expect(subject).not_to eq(described_class.new(name: "different")) }
+      it { expect(subject).not_to eq(described_class.new(id: 2, short_name: "different", name: "2 - different")) }
     end
   end
 end
