17ef67dbc2ba81bafc9edd26a2ff69d39d7355b4
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2026-09-14 13:28:17 +0100

    merge app store method for claim types here

--- lib/laa_crime_forms_common/payment_family.rb
+++ lib/laa_crime_forms_common/payment_family.rb
@@ -1,12 +1,32 @@
 module LaaCrimeFormsCommon
-  module PaymentHelper
-    def get_parent(request_type)
-      if request_type.start_with?("non_standard_mag")
-        "non_standard_magistrate"
-      elsif request_type.start_with?("assigned_counsel")
-        "assigned_counsel"
+  module PaymentFamily
+    CLAIM_TYPE_MAP = {
+      "NsmClaim" => %w[
+        breach_of_injunction
+        non_standard_magistrate
+        non_standard_mag_supplemental
+        non_standard_mag_appeal
+        non_standard_mag_amendment
+      ],
+      "AssignedCounselClaim" => %w[
+        assigned_counsel
+        assigned_counsel_appeal
+        assigned_counsel_amendment
+      ],
+    }.freeze
+
+    def find_claim_type_group(request_type)
+      CLAIM_TYPE_MAP.find { |_, types| types.include?(request_type) }&.first
+    end
+
+    def get_claim_type_marker(request_type)
+      case find_claim_type_group(request_type)
+      when "NsmClaim"
+        "nsm_claim"
+      when "AssignedCounselClaim"
+        "assigned_counsel_claim"
       else
-        raise "Unknown request type"
+        raise StandardError, "request type '#{request_type}' is not recognized"
       end
     end
 
